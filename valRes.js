f9cb878962ff29819339046aa1817485e89a58df
Morgan Lindquist <djspaken@gmail.com>
2019-05-01 19:25:39 +0200

    polis-change

--- valRes.js
+++ valRes.js
@@ -1,5 +1,6 @@
 var value = sessionStorage.getItem('sliderValue');
+var op = 100 - value;
 var text1 = 'Du \344r ';
-var text2 = ' % robot!'
-var text3 = text1.concat(value).concat(text2);
+var text2 = ' % kriminell!'
+var text3 = text1.concat(op).concat(text2);
 document.getElementById('result').innerHTML = text3;
